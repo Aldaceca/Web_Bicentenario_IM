8ccd822f30567967faa4e2942d28d15c6abd83a9
camilo-ramirez-1211 <80415877+camilo-ramirez-1211@users.noreply.github.com>
2021-05-20 18:38:50 -0500

    Blog Re Funcional Con Imagenes,Link, y nombre oficial, no deja poblicar si no esta iniciada la sesion

--- ProyectTecnologias1.0/app.js
+++ ProyectTecnologias1.0/app.js
@@ -10,26 +10,51 @@ var firebaseConfig = {
 };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/firebase.User
+          var uid = user.uid;
+          var db = firebase.firestore();         
+          var user = firebase.auth().currentUser;         
+                  
+
+
+             
+      } else {
+        document.getElementById('crear').href = "index.html";
+        document.getElementById('crear').innerHTML = `<P>Iniciar Sesion `;
+      }
+  });
   
   let postCollection = document.querySelector("#posts-collection");
   
   const db = firebase.firestore();
   
-  function createPost(title, time, content) {
+  function createPost(title, imagen,link,autor, content) {
     let div = document.createElement("div");
     div.setAttribute("class", "col-md-12");
   
     let h2 = document.createElement("h2");
     let p = document.createElement("p");
-    let small = document.createElement("small");
-  
+    let img = document.createElement("img");
+    let small = document.createElement("a");
+    let p2 = document.createElement("p");
+    
     h2.textContent = title;
-    small.textContent = time;
+    img.src=imagen;
     p.textContent = content;
+    small.textContent = "Linkeding";
+    small.href = link;
+    p2.textContent = autor;
   
-    div.appendChild(h2);
+    div.appendChild(h2);    
     div.appendChild(small);
-    div.appendChild(p);
+     div.appendChild(p2);
+    div.appendChild(p);    
+     div.appendChild(img);
+     
   
     postCollection.appendChild(div);
   }
@@ -42,7 +67,9 @@ var firebaseConfig = {
         snapshot.docs.forEach(docs => {
           createPost(
             docs.data().postName,
-            docs.data().createdAt,
+            docs.data().img,
+            docs.data().link,
+            docs.data().author,
             docs.data().postContent
           );
         });
--- ProyectTecnologias1.0/create.js
+++ ProyectTecnologias1.0/create.js
@@ -1,23 +1,93 @@
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+   
+var storage = firebase.storage();
+var user = firebase.auth().currentUser;
+var uploader = document.getElementById("uploader");
+var fileButton = document.getElementById("foimg");
+var foto="";
 
-document.querySelector("#submitBtn").addEventListener("click", function() {
-    let postAuthor = document.querySelector("#author").value;
-    let postTitle = document.querySelector("#postTitle").value;
-    let postContent = document.querySelector("#postContent").value;
-    let postDate = document.querySelector("#postDate").value;
-  
-    if (
-      postAuthor === "" ||
-      postTitle === "" ||
-      postContent === "" ||
-      postDate === ""
-    ) {
-      alert("Fields Empty");
-    } else {
+fileButton.addEventListener('change', function (e) {
+  // File or Blob named mountains.jpg
+  var file = e.target.files[0];
+
+  // Create the file metadata
+  var metadata = {
+    contentType: 'image/jpeg'
+  };
+
+  // Upload file and metadata to the object 'images/mountains.jpg'
+  var storageRef = firebase.storage().ref('fotos/' + file.name);
+  var uploadTask = storageRef.put(file, metadata);
+
+  // Listen for state changes, errors, and completion of the upload.
+  uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+    function (snapshot) {
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          console.log('Upload is paused');
+          break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log('Upload is running');
+          break;
+      }
+    }, function (error) {
+
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case 'storage/unauthorized':
+          // User doesn't have permission to access the object
+          break;
+
+        case 'storage/canceled':
+          // User canceled the upload
+          break;
+        case 'storage/unknown':
+          // Unknown error occurred, inspect error.serverResponse
+          break;
+      }
+    }, function () {
+      // Upload completed successfully, now we can get the download URL
+      uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+        console.log('File available at', downloadURL);
+        foto = downloadURL;
+        console.log('la foto ', foto);
+      });
+    });
+
+});
+
+document.querySelector("#submitBtn").addEventListener("click", function () {
+
+  let postTitle = document.querySelector("#postTitle").value;
+  let postContent = document.querySelector("#postContent").value;
+  let link = document.getElementById("link").value;
+  let img = foto;
+  console.log("foto cargada "+img);
+  if (
+    
+    postTitle === "" ||
+    postContent === ""
+  ) {
+    alert("Fields Empty");
+  } else {
+   
       db.collection("posts").doc().set({
-          author: postAuthor,
-          createdAt: postDate,
-          postName: postTitle,
-          postContent: postContent
-        });
-    }
-  });
+        author: user.displayName,        
+        postName: postTitle,
+        img: img,
+        link: link,
+        postContent: postContent
+        
+      });
+   
+   
+  }
+});
+           
+    } 
+});
