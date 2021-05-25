f544c8708c43ab305f31ce07455449112107d3f9
camilo-ramirez-1211 <80415877+camilo-ramirez-1211@users.noreply.github.com>
2021-05-25 15:13:37 -0500

    ultima version blog full

--- ProyectTecnologias1.0/app.js
+++ ProyectTecnologias1.0/app.js
@@ -8,82 +8,86 @@ var firebaseConfig = {
     appId: "1:136457534820:web:f640bcb9a8479ccc338e71",
     measurementId: "G-DTEQ21EP3J"
 };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-  firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-          // User is signed in, see docs for a list of available properties
-          // https://firebase.google.com/docs/reference/js/firebase.User
-          var uid = user.uid;
-          var db = firebase.firestore();         
-          var user = firebase.auth().currentUser;         
-                  
-
-
-             
-      } else {
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        var uid = user.uid;
+        var db = firebase.firestore();
+        var user = firebase.auth().currentUser;
+    } else {
         document.getElementById('crear').href = "index.html";
-        document.getElementById('crear').innerHTML = `<P>Iniciar Sesion `;
-      }
-  });
-  
-  let postCollection = document.querySelector("#posts-collection");
-  
-  const db = firebase.firestore();
-  
-  function createPost(title, imagen,link,autor, content) {
-    let div = document.createElement("div");
-    div.setAttribute("class", "row col-md-12");
+        document.getElementById('crear').innerHTML = `Iniciar sesión`;
+    }
+});
 
+let postCollection = document.querySelector("#posts-collection");
+
+const db = firebase.firestore();
+
+function createPost(title, imagen, link, autor, content) {
+    let div = document.createElement("div");
+    div.setAttribute("class", "publicacion row col-md-12");
+    let div2 = document.createElement("div");
+    div2.setAttribute("class", "publicacion col-md-12");
 
     let h2 = document.createElement("h2");
     let p = document.createElement("p");
     let img = document.createElement("img");
+    img.setAttribute("class", "padd-img");
     let small = document.createElement("a");
+    small.setAttribute("class", "btn btn-outline-primary col-sm-2");
     let p2 = document.createElement("p");
-    let espacio=document.createElement("p")
-    
+    let espacio = document.createElement("p")
+
     h2.textContent = title;
-    img.src=imagen;
+    img.src = imagen;
     p.textContent = content;
-    small.textContent = "Linkeding";
+    small.textContent = "LinkedIn";
     small.href = link;
     p2.textContent = autor;
-    espacio.textContent="-------------------------------------------------------------------------------------------------------------------------------";
-  
-    div.appendChild(h2);    
-    div.appendChild(small);
-     div.appendChild(p2);
-    div.appendChild(p);    
-     div.appendChild(img);
-     div.appendChild(espacio);
-     
-    
-    
-     
-  
+    espacio.textContent = "";
+    // div2.appendChild(
+
+    div.appendChild(h2),
+        div.appendChild(p2),
+        div.appendChild(p),
+        div.appendChild(small),
+        div.appendChild(img),
+        div.appendChild(div2)
+        //  ); 
+
+
+
+
     postCollection.appendChild(div);
-  }
-  
-  // Get Posts
-  function getPosts() {
+}
+
+// Get Posts
+function getPosts() {
+    let div = document.createElement("div");
+    div.setAttribute("class", "row col-md-12");
+
     db.collection("posts")
-      .get()
-      .then(snapshot => {
-        snapshot.docs.forEach(docs => {
-          createPost(
-            docs.data().postName,
-            docs.data().img,
-            docs.data().link,
-            docs.data().author,
-            docs.data().postContent
-          );
+        .orderBy("date", "desc")
+        .get()
+        .then(snapshot => {
+            snapshot.docs.forEach(docs => {
+                createPost(
+                    docs.data().postName,
+                    docs.data().img,
+                    docs.data().link,
+                    docs.data().author,
+                    docs.data().postContent
+                );
+            });
+        })
+        .catch(err => {
+            console.log(err);
         });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-  
-  getPosts();
+}
+
+getPosts();
--- ProyectTecnologias1.0/create.js
+++ ProyectTecnologias1.0/create.js
@@ -1,94 +1,100 @@
 firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-   
-var storage = firebase.storage();
-var user = firebase.auth().currentUser;
-var uploader = document.getElementById("uploader");
-var fileButton = document.getElementById("foimg");
-var foto="";
+    if (user) {
 
-fileButton.addEventListener('change', function (e) {
-  // File or Blob named mountains.jpg
-  var file = e.target.files[0];
+        var storage = firebase.storage();
+        var user = firebase.auth().currentUser;
+        var uploader = document.getElementById("uploader");
+        var fileButton = document.getElementById("foimg");
+        var foto = "";
 
-  // Create the file metadata
-  var metadata = {
-    contentType: 'image/jpeg'
-  };
+        fileButton.addEventListener('change', function(e) {
+            // File or Blob named mountains.jpg
+            var file = e.target.files[0];
 
-  // Upload file and metadata to the object 'images/mountains.jpg'
-  var storageRef = firebase.storage().ref('fotos/' + file.name);
-  var uploadTask = storageRef.put(file, metadata);
+            // Create the file metadata
+            var metadata = {
+                contentType: 'image/jpeg'
+            };
 
-  // Listen for state changes, errors, and completion of the upload.
-  uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-    function (snapshot) {
-      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log('Upload is ' + progress + '% done');
-      switch (snapshot.state) {
-        case firebase.storage.TaskState.PAUSED: // or 'paused'
-          console.log('Upload is paused');
-          break;
-        case firebase.storage.TaskState.RUNNING: // or 'running'
-          console.log('Upload is running');
-          break;
-      }
-    }, function (error) {
+            // Upload file and metadata to the object 'images/mountains.jpg'
+            var storageRef = firebase.storage().ref('fotos/' + file.name);
+            var uploadTask = storageRef.put(file, metadata);
 
-      // A full list of error codes is available at
-      // https://firebase.google.com/docs/storage/web/handle-errors
-      switch (error.code) {
-        case 'storage/unauthorized':
-          // User doesn't have permission to access the object
-          break;
+            // Listen for state changes, errors, and completion of the upload.
+            uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+                function(snapshot) {
+                    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log('Upload is ' + progress + '% done');
+                    switch (snapshot.state) {
+                        case firebase.storage.TaskState.PAUSED: // or 'paused'
+                            console.log('Upload is paused');
+                            break;
+                        case firebase.storage.TaskState.RUNNING: // or 'running'
+                            console.log('Upload is running');
+                            break;
+                    }
+                },
+                function(error) {
 
-        case 'storage/canceled':
-          // User canceled the upload
-          break;
-        case 'storage/unknown':
-          // Unknown error occurred, inspect error.serverResponse
-          break;
-      }
-    }, function () {
-      // Upload completed successfully, now we can get the download URL
-      uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-        console.log('File available at', downloadURL);
-        foto = downloadURL;
-        console.log('la foto ', foto);
-      });
-    });
+                    // A full list of error codes is available at
+                    // https://firebase.google.com/docs/storage/web/handle-errors
+                    switch (error.code) {
+                        case 'storage/unauthorized':
+                            // User doesn't have permission to access the object
+                            break;
 
-});
+                        case 'storage/canceled':
+                            // User canceled the upload
+                            break;
+                        case 'storage/unknown':
+                            // Unknown error occurred, inspect error.serverResponse
+                            break;
+                    }
+                },
+                function() {
+                    // Upload completed successfully, now we can get the download URL
+                    uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                        console.log('File available at', downloadURL);
+                        foto = downloadURL;
+                        console.log('la foto ', foto);
+                    });
+                });
 
-document.querySelector("#submitBtn").addEventListener("click", function () {
+        });
 
-  let postTitle = document.querySelector("#postTitle").value;
-  let postContent = document.querySelector("#postContent").value;
-  let link = document.getElementById("link").value;
-  let img = foto;
-  console.log("foto cargada "+img);
-  if (
-    
-    postTitle === "" ||
-    postContent === ""
-  ) {
-    alert("Fields Empty");
-  } else {
-   
-      db.collection("posts").doc().set({
-        author: user.displayName,        
-        postName: postTitle,
-        img: img,
-        link: link,
-        postContent: postContent
-        
-        
-      });
-   
-      document.getElementById('publ').innerHTML = `<P>Publicacion Exitosa `  + `<P> <button  class="btn btn-primary rounded-pill" onclick="location.href='blog.html'">Ir Al Blog</button>` + '<p>';
-  }
-});
-           
-    } 
+        document.querySelector("#submitBtn").addEventListener("click", function() {
+
+            let postTitle = document.querySelector("#postTitle").value;
+            let postContent = document.querySelector("#postContent").value;
+            let link = document.getElementById("link").value;
+            let date = document.getElementById("theDate").value;
+            // let hour = document.getElementById("theHour").value;
+            let img = foto;
+            console.log("foto cargada " + img);
+            if (
+
+                postTitle === "" ||
+                postContent === ""
+            ) {
+                alert("Fields Empty");
+            } else {
+
+                db.collection("posts").doc().set({
+                    author: user.displayName,
+                    postName: postTitle,
+                    img: img,
+                    link: link,
+                    postContent: postContent,
+                    date: date
+                  
+
+
+                });
+
+                document.getElementById('publ').innerHTML = `<P>Publicacion Exitosa ` + `<P> <button  class="btn btn-primary rounded-pill" onclick="location.href='blog.html'">Ir Al Blog</button>` + '<p>';
+            }
+        });
+
+    }
 });
